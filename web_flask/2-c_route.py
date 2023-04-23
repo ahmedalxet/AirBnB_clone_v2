d8d61ee9a190f1b28d7821b99be0d7a3db78576b
ahmedalxet <ahmelaq@gmail.com>
2023-04-23 05:23:26 +0300

    correction 3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,18 +1,27 @@
+#!/usr/bin/python3
+"""Module to start a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Route that displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Route that displays 'HBNB'"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
+def c_text(text):
+    """Route that displays 'C ' followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

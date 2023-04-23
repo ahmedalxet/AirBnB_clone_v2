b7333676fa359376154f6b52ca7bd18cd37653aa
ahmedalxet <ahmelaq@gmail.com>
2023-04-23 06:44:38 +0300

    correciton

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
+# This script sets up the web servers for the deployment of web_static.
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null
-then
-    sudo apt-get update
-    sudo apt-get install -y nginx
+# Install Nginx if it not already installed
+if ! which nginx > /dev/null; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
 fi
 
-# Create necessary folders
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/current/
+# Create the necessary directories if they don't already exist
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-# Create a fake HTML file
-sudo echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+# Create a fake HTML file in /data/web_static/releases/test
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create symbolic link
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+# Create a symbolic link to /data/web_static/releases/test/
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test /data/web_static/current
 
-# Set ownership of /data/ folder to ubuntu user and group
-sudo chown -R ahmed:ahmed /data/
+# Give ownership of the /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
